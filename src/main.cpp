eac8c669d99c480840eb552addf037b03c441646
fixshortcut <faqihthefirst@gmail.com>
2022-09-29 15:56:20 +0700

    add parsing data

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@
 #define rst 14
 #define dio0 2
 
+String deviceRX = "SensDev", sens1 = "S1 = ", sens2 = "S2 = ";
+String LoRaData, sen1, sen2;
+int sensor1, sensor2;
+
 void setup() {
   //initialize Serial Monitor
   Serial.begin(115200);
@@ -40,12 +44,21 @@ void loop() {
 
     // read packet
     while (LoRa.available()) {
-      String LoRaData = LoRa.readString();
+      LoRaData = LoRa.readString();
       Serial.print(LoRaData); 
+      int indexS1 = LoRaData.indexOf(sens1);
+      int indexS2 = LoRaData.indexOf(sens2);
+      int indexComma = LoRaData.indexOf(",");
+  
+      sen1 = LoRaData.substring(indexS1 + sens1.length(),indexComma);
+      sen2 = LoRaData.substring(indexS2 + sens2.length(),indexS2 + sens2.length()+ 3);
     }
+    
+
 
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
+    Serial.printf("sensor1 = %s , sensor2 = %s \n",sen1,sen2);
   }
 }
